0c2dae874f6977f8e78b180c9d8acff0289382f6
admiswalker <admiswalker@gmail.com>
2023-05-20 15:01:59 +0900

    wip

--- lib/aws-cdk-template-ver-single-stack-v2-stack.ts
+++ lib/aws-cdk-template-ver-single-stack-v2-stack.ts
@@ -1,19 +1,97 @@
-import { Duration, Stack, StackProps } from 'aws-cdk-lib';
-import * as sns from 'aws-cdk-lib/aws-sns';
-import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
-import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as fs from 'fs';
+import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import { InstanceType, NatInstanceImage, NatProvider } from 'aws-cdk-lib/aws-ec2';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import { Stack, StackProps } from 'aws-cdk-lib';
 
-export class AwsCdkTemplateVerSingleStackV2Stack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+interface AwsCdkTplStackProps extends StackProps {
+}
+export class AwsCdkTplStack extends Stack {
+  constructor(scope: Construct, id: string, props?: AwsCdkTplStackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'AwsCdkTemplateVerSingleStackV2Queue', {
-      visibilityTimeout: Duration.seconds(300)
+    // VPC
+    const nat_instance = ec2.NatProvider.instance({
+      instanceType: new InstanceType('t3a.nano'),
+      machineImage: new NatInstanceImage(),
+      defaultAllowedTraffic: ec2.NatTrafficDirection.OUTBOUND_ONLY,
+    });
+    const vpc = new ec2.Vpc(this, this.constructor.name+'-vpc_for_ec2_and_ssm', {
+      ipAddresses: ec2.IpAddresses.cidr('10.0.0.0/16'),
+      natGateways: 1,
+      natGatewayProvider: nat_instance,
+      subnetConfiguration: [
+        {
+          name: 'Public',
+          subnetType: ec2.SubnetType.PUBLIC,
+          cidrMask: 27,
+        },
+        {
+          name: 'Private',
+          subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS,
+          cidrMask: 27,
+        },
+      ],
     });
 
-    const topic = new sns.Topic(this, 'AwsCdkTemplateVerSingleStackV2Topic');
+    // SSM
+    const ssm_iam_role = new iam.Role(this, this.constructor.name+'-iam_role_for_ssm', {
+      assumedBy: new iam.ServicePrincipal('ec2.amazonaws.com'),
+      managedPolicies: [
+        iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonSSMManagedInstanceCore'),
+        iam.ManagedPolicy.fromAwsManagedPolicyName('CloudWatchAgentAdminPolicy'),
+      ],
+    });
 
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    vpc.addInterfaceEndpoint(this.constructor.name+'-InterfaceEndpoint_ssm', {
+      service: ec2.InterfaceVpcEndpointAwsService.SSM,
+    });
+    vpc.addInterfaceEndpoint(this.constructor.name+'-InterfaceEndpoint_ec2_messages', {
+      service: ec2.InterfaceVpcEndpointAwsService.EC2_MESSAGES,
+    });
+    vpc.addInterfaceEndpoint(this.constructor.name+'-InterfaceEndpoint_ssm_messages', {
+      service: ec2.InterfaceVpcEndpointAwsService.SSM_MESSAGES,
+    });
+
+    // EC2
+    const cloud_config = ec2.UserData.forLinux({shebang: ''})
+    const user_data_script = fs.readFileSync('./lib/ec2_user-data.yaml', 'utf8');
+    cloud_config.addCommands(user_data_script)
+    const multipartUserData = new ec2.MultipartUserData();
+    multipartUserData.addPart(ec2.MultipartBody.fromUserData(cloud_config, 'text/cloud-config; charset="utf8"'));
+
+    const ec2_sg = new ec2.SecurityGroup(this, 'Ec2Sg', {
+      allowAllOutbound: true,
+      securityGroupName: 'EC2 Sev Security Group',
+      vpc: vpc,
+    });
+    
+    const ec2_instance = new ec2.Instance(this, this.constructor.name+'-general_purpose_ec2', {
+      instanceType: new ec2.InstanceType('t3a.nano'), // 2 vCPU, 0.5 GB
+//    machineImage: ec2.MachineImage.genericLinux({'us-west-2': 'ami-XXXXXXXXXXXXXXXXX'}),
+      machineImage: new ec2.AmazonLinuxImage({
+        generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX,
+        edition: ec2.AmazonLinuxEdition.STANDARD,
+        virtualization: ec2.AmazonLinuxVirt.HVM,
+        storage: ec2.AmazonLinuxStorage.GENERAL_PURPOSE,
+      }),
+      vpc: vpc,
+//    blockDevices: [{
+//	    deviceName: '/dev/sda1',
+//	    volume: ec2.BlockDeviceVolume.ebs(30),
+//    }],
+      vpcSubnets: vpc.selectSubnets({
+        subnetGroupName: 'Private',
+      }),
+      role: ssm_iam_role,
+      userData: multipartUserData,
+      securityGroup: ec2_sg,
+    });
+
+    nat_instance.connections.allowFrom(ec2_sg, ec2.Port.allTraffic());
+
+    //---
   }
 }
